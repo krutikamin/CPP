26439aac2da17d7affc88611dfca466aa1421fb0
krutikamin <krutikamin@csu.fullerton.edu>
2015-12-23 02:40:47 -0800

    Modified problem-5.cpp

--- Problem_Solving_With_C++_by_Walter_Savitch_Eighth_Edition/Chapter-1/problem-5.cpp
+++ Problem_Solving_With_C++_by_Walter_Savitch_Eighth_Edition/Chapter-1/problem-5.cpp
@@ -1,10 +1,10 @@
 //----------------------------------------------------------------------------------------------//
-//	Name -			problem-5.cpp																//
-//	Description -	Write a c++ programme that reads in two integers and then outputs both		//
-//					their sum and their product.												//
-//	Created -		23/12/2015																	//
-//	Last Modified -	23/12/2015																	//
-//	Errors -		None																		//
+//	Name -			problem-5.cpp							//
+//	Description -		Write a c++ programme that reads in two integers and then 	//
+//				outputs both their sum and their product.			//
+//	Created -		23/12/2015							//
+//	Last Modified -		23/12/2015							//
+//	Errors -		None								//
 //----------------------------------------------------------------------------------------------//
 
 #include <iostream>
@@ -12,7 +12,7 @@ using namespace std;
 
 int main(void) 
 {
-	int		integerOne, integerTwo;
+	int	integerOne, integerTwo;
 
 	cout << "Please enter the first integer : ";
 	cin >> integerOne;
@@ -27,7 +27,6 @@ int main(void)
 	cout << "This is the end of the program.\n";
 	cout << "Press any key to continue...";
 	cin.get();
-	getchar();
 	
 	return 0;
 }
