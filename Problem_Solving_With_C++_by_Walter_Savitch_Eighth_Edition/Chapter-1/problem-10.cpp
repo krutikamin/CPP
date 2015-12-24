6d6f660dcfbc702b5a4a80e9100a0c1989394c39
krutikamin <krutikamin@csu.fullerton.edu>
2015-12-23 18:06:48 -0800

    Added problem-10.cpp

---
+++ Problem_Solving_With_C++_by_Walter_Savitch_Eighth_Edition/Chapter-1/problem-10.cpp
@@ -0,0 +1,40 @@
+//----------------------------------------------------------------------------------------------//
+//	Name -		problem-10.cpp								//
+//	Description -	Write a programme that inputs a character from the keyboard and then 	//
+//			outputs a large block letter "C" composed of that character.		//
+//	Created -	24/12/2015								//
+//	Last Modified -	24/12/2015								//
+//	Errors -	None									//
+//	Sample Run -										//
+//			Please enter the time in seconds: 2					//
+//			The object would drop about 64 feet in freefall.			//
+//			Press any key to continue...						//
+//----------------------------------------------------------------------------------------------//
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	char myChar;
+
+	cout << "Please enter the character to display: ";
+	cin >> myChar;
+	cout << "----------------------------------------------\n";
+	cout << "   " << myChar << " " << myChar << " " << myChar << endl;
+	cout << "  " << myChar << "     " << myChar << endl;
+	cout << " " << myChar << endl;
+	cout << " " << myChar << endl;
+	cout << " " << myChar << endl;
+	cout << " " << myChar << endl;
+	cout << " " << myChar << endl;
+	cout << "  " << myChar << "     " << myChar << endl;
+	cout << "   " << myChar << " " << myChar << " " << myChar << endl ;
+	cout << "----------------------------------------------\n";
+
+	cout << "Press any key to continue...";
+	cin.get();
+	getchar();
+
+	return 0;
+}
