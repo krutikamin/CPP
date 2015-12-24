04a4a3d2a5dea912bc49b3290481854b73c4f7bf
krutikamin <krutikamin@csu.fullerton.edu>
2015-12-23 16:22:55 -0800

    Added problem-7.cpp

---
+++ Problem_Solving_With_C++_by_Walter_Savitch_Eighth_Edition/Chapter-1/problem-7.cpp
@@ -0,0 +1,33 @@
+//----------------------------------------------------------------------------------------------//
+//	Name -		problem-5.cpp								//
+//	Description -	Write a c++ programme that prints out "C S !" in large block letters	// 
+//			inside a border of *s followed by two lines then the message Computer	//
+//			Computer Science is Cool Stuff						//
+//	Created -	23/12/2015								//
+//	Last Modified -	23/12/2015								//
+//	Errors -	None									//
+//----------------------------------------------------------------------------------------------//
+
+#include <iostream>
+using namespace std;
+
+int main(void) 
+{
+	cout << "*****************************************************\n\n";
+	cout << "       C C C           S S S S       !!\n";
+	cout << "      C      C        S        S     !!\n";
+	cout << "     C               S               !!\n";
+	cout << "    C                 S              !!\n";
+	cout << "    C                  S S S S       !!\n";
+	cout << "    C                          S     !!\n";
+	cout << "     C                          S    !!\n";
+	cout << "      C      C        S        S       \n";
+	cout << "       C C C            S S S S      00\n\n";
+	cout << "*****************************************************\n\n";
+	cout << "   Computer Science is Cool Stuff!!!\n\n";
+	
+	cout << "Press any key to continue...";
+	cin.get();
+	
+	return 0;
+}
