f38a89f6f05d9dc68af64f2fbc9513e38152ef70
krutikamin <krutikamin@csu.fullerton.edu>
2016-01-12 21:13:42 -0800

    Created problem-3.cpp

---
+++ Problem_Solving_With_C++_by_Walter_Savitch_Eighth_Edition/Chapter-2/problem-3.cpp
@@ -0,0 +1,33 @@
+//----------------------------------------------------------------------------------------------//
+//	Name -		problem-3.cpp								//
+//	Description -	Workers at particular company have won a 7.6% pay increase retroactive	//
+//			for 6 months. Write a program that takes an employee's previous annual	//
+//			alary as input, and outputs the amount of retroactive pay due the  	//
+//			employee, the new annual salary, and the new monthly salary.		//
+//	Created -	12/1/2016								//
+//	Last Modified -	12/1/2016								//
+//	Errors -	None									//
+//	Sample Run -										//
+//			Please enter the weight of a breakfast cereal in ounces: 56466		//
+//			The total weight in metric ton is 1.60079				//
+//			1959 boxes will be required to yield 1 metric ton of			//
+//			cereal assuming a box of cereal weighs 18 oz.				//
+//			----------------------------------------------------------------	//
+//			Would you like to calculate again?(y/n) y				//
+//			Please enter the weight of a breakfast cereal in ounces: 23656		//
+//			The total weight in metric ton is 0.670637				//
+//			1959 boxes will be required to yield 1 metric ton of			//
+//			cereal assuming a box of cereal weighs 18 oz.				//
+//			----------------------------------------------------------------	//
+//			Would you like to calculate again?(y/n) n				//
+//			Press any key to continue...						//
+//----------------------------------------------------------------------------------------------//
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	const	double 	payIncrease 	7.6;
+	int	salary;	
+}
