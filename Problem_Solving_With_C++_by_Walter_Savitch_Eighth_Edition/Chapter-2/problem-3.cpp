971be7ed7e5bc35116a0cd5a85e59556e2691361
krutikamin <krutikamin@csu.fullerton.edu>
2016-01-13 23:02:51 -0800

    None

--- Problem_Solving_With_C++_by_Walter_Savitch_Eighth_Edition/Chapter-2/problem-3.cpp
+++ Problem_Solving_With_C++_by_Walter_Savitch_Eighth_Edition/Chapter-2/problem-3.cpp
@@ -5,22 +5,9 @@
 //			alary as input, and outputs the amount of retroactive pay due the  	//
 //			employee, the new annual salary, and the new monthly salary.		//
 //	Created -	12/1/2016								//
-//	Last Modified -	12/1/2016								//
+//	Last Modified -	13/1/2016								//
 //	Errors -	None									//
 //	Sample Run -										//
-//			Please enter the weight of a breakfast cereal in ounces: 56466		//
-//			The total weight in metric ton is 1.60079				//
-//			1959 boxes will be required to yield 1 metric ton of			//
-//			cereal assuming a box of cereal weighs 18 oz.				//
-//			----------------------------------------------------------------	//
-//			Would you like to calculate again?(y/n) y				//
-//			Please enter the weight of a breakfast cereal in ounces: 23656		//
-//			The total weight in metric ton is 0.670637				//
-//			1959 boxes will be required to yield 1 metric ton of			//
-//			cereal assuming a box of cereal weighs 18 oz.				//
-//			----------------------------------------------------------------	//
-//			Would you like to calculate again?(y/n) n				//
-//			Press any key to continue...						//
 //----------------------------------------------------------------------------------------------//
 
 #include <iostream>
@@ -30,4 +17,5 @@ int main(void)
 {
 	const	double 	payIncrease 	7.6;
 	int	salary;	
+
 }
