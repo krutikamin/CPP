76e23a63f35fe417a00ebdf4332950d0a77dee3b
krutikamin <krutikamin@csu.fullerton.edu>
2017-03-24 14:17:31 -0700

    finished problem-3.cpp

--- Problem_Solving_With_C++_by_Walter_Savitch_Eighth_Edition/Chapter-2/problem-3.cpp
+++ Problem_Solving_With_C++_by_Walter_Savitch_Eighth_Edition/Chapter-2/problem-3.cpp
@@ -5,7 +5,7 @@
 //			alary as input, and outputs the amount of retroactive pay due the  	//
 //			employee, the new annual salary, and the new monthly salary.		//
 //	Created -	12/1/2016								//
-//	Last Modified -	13/1/2016								//
+//	Last Modified -	24/3/2017								//
 //	Errors -	None									//
 //	Sample Run -										//
 //----------------------------------------------------------------------------------------------//
@@ -15,7 +15,22 @@ using namespace std;
 
 int main(void)
 {
-	const	double 	payIncrease 	7.6;
-	int	salary;	
+	const	double 	payIncrease = 7.6;
+	double	previous_salary;	
+	double	new_salary;	
+	double	temp;
 
+	cout << "Please enter your annual salary - $";
+	cin >> previous_salary;
+	cout << endl;
+
+	temp = previous_salary / 2;
+	cout << "Your retroactive pay due = $" << (temp*payIncrease) / 100 << endl;;
+
+	new_salary = previous_salary + ((previous_salary * payIncrease) / 100);
+
+	cout << "Your new annual salary will be $" << new_salary << endl; 
+	cout << "Your new monthly salary will be $" << new_salary / 12 << endl;
+	
+	return 0;
 }
